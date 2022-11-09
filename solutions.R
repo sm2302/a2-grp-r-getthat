3dfb065deddc9bce562141c2f13adc19f2b4261a
AfiqahSofry <19b2102@ubd.edu.bn>
2022-11-09 23:02:22 +0800

    method A codes

--- solutions.R
+++ solutions.R
@@ -48,23 +48,60 @@ ggplot(data = data_circle, aes(x = x0, y = y0)) +
 library(tidyverse)
 library(ggforce)
 library(ggplot2)
+library(dplyr)
 
-theta <- runif(100, 0, 2*pi)
-circle <- tibble(
-  x0 = cos(theta),
-  y0 = sin(theta),
-  r = 1)
+n <- 1000 #number of chords - can change this
+
+theta_1 <- runif(n, 0, 2*pi)
+endpoint_1 <- tibble(
+  x0 = cos(theta_1),
+  y0 = sin(theta_1))
+
+theta_2 <- runif(n, 0, 2*pi)
+endpoint_2 <- tibble(
+  x1 = cos(theta_2),
+  y1 = sin(theta_2))
+
+circle <- bind_cols(endpoint_1, endpoint_2)
 
 eqtri_df <- tibble(
   x    = c(0, sqrt(3) / 2, -sqrt(3) / 2),
   y    = c(1, -0.5, -0.5),
   xend = c(sqrt(3) / 2, -sqrt(3) / 2, 0),
-  yend = c(-0.5, -0.5, 1)
-)
+  yend = c(-0.5, -0.5, 1))
 
 ggplot(data = circle, mapping = aes(x = x0, y = y0)) +
   geom_point() + 
-  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend, color = "red"))
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend), color = "red") 
+
+  #+ geom_segment(data = circle, aes(x0 = x0, y0 = y0, x1= x1, y1 = y1)) +
+  #coord_equal()
+
+circle_new <- mutate(.data = circle, l = sqrt((x1 - x0)^2 + (y1 - y0)^2)) # add new column l
+
+eqtri_df_new <- mutate(.data = eqtri_df, s = sqrt((xend - x)^2 + (yend - y)^2)) # add new column s
+
+final_eqtri_df_new <- slice(.data = eqtri_df_new, 1)
+
+s <- last_col_eqtri_df_new <- final_eqtri_df_new$s
+
+final_table <- bind_cols(circle_new, s)
+colnames(final_table)[6]  <- "s"
+
+compare <- select(.data = final_table, l, s)
+result <- filter(.data = compare, l > s)
+print(result)
+print(nrow(result))
+
+#ggplot(data = circle, mapping = aes(x = x0, y = y0)) +
+ # geom_point() + 
+#  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend), color = "red") 
+ #+ geom_segment(data = circle, aes(x0 = x0, y0 = y0, x1= x1, y1 = y1)) +
+ #coord_equal()
+
+# Using Principle of Indifference
+prob = nrow(result)/n
+print(prob)
 
 # 1. Select the random endpoints directly from the dataframe
 circle[1:2,]
