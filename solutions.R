537b4c9903482a3ee6d504fe83bd6ee443150401
AfiqahSofry <19b2102@ubd.edu.bn>
2022-11-10 14:12:38 +0800

    update method A

--- solutions.R
+++ solutions.R
@@ -53,16 +53,12 @@ library(dplyr)
 n <- 150 #number of chords - can change this
 
 theta_1 <- runif(n, 0, 2*pi)
-endpoint_1 <- tibble(
-  x0 = cos(theta_1),
-  y0 = sin(theta_1))
-
 theta_2 <- runif(n, 0, 2*pi)
-endpoint_2 <- tibble(
-  x1 = cos(theta_2),
-  y1 = sin(theta_2))
-
-circle <- bind_cols(endpoint_1, endpoint_2)
+endpoint <- tibble(
+  x1 = cos(theta_1),
+  y1 = sin(theta_1),
+  x2 = cos(theta_2),
+  y2 = sin(theta_2))
 
 eqtri_df <- tibble(
   x    = c(0, sqrt(3) / 2, -sqrt(3) / 2),
@@ -78,7 +74,7 @@ eqtri_df <- tibble(
   #+ geom_segment(data = circle, aes(x0 = x0, y0 = y0, x1= x1, y1 = y1)) +
   #coord_equal()
 
-circle_new <- mutate(.data = circle, l = sqrt((x1 - x0)^2 + (y1 - y0)^2)) # add new column l
+circle <- mutate(.data = endpoint, l = sqrt((x2 - x1)^2 + (y2 - y1)^2)) # add new column l
 
 eqtri_df_new <- mutate(.data = eqtri_df, s = sqrt((xend - x)^2 + (yend - y)^2)) # add new column s
 
@@ -86,7 +82,7 @@ final_eqtri_df_new <- slice(.data = eqtri_df_new, 1) # only need 1 row as refere
 
 s <- last_col_eqtri_df_new <- final_eqtri_df_new$s
 
-final_table <- bind_cols(circle_new, s)
+final_table <- bind_cols(circle, s)
 colnames(final_table)[6]  <- "s"
 
 compare <- select(.data = final_table, l, s)
@@ -96,33 +92,33 @@ shorter <- filter(.data = final_table, l < s)
 print(result)
 chords_longer <- print(nrow(result))
 
-ggplot(data = circle, mapping = aes(x = x0, y = y0)) +
+ggplot(data = endpoint, aes(x = x1, y = y1)) +
+  #ggplot(data = data_circle, aes(x = x0, y = y0)) +
+  #geom_circle(data = data_circle, aes(x0 = x0, y0 = y0, r=r)) +
   geom_point() + 
-  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend), col = "red") +
-  geom_segment(data = longer , aes(x = x0, y = y0, xend = x1, yend = y1), col = "blue") + 
-  geom_segment(data = shorter , aes(x = x0, y = y0, xend = x1, yend = y1), col = "green") 
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend), col = "red", lwd = 1.5) +
+  geom_segment(data = longer , aes(x = x1, y = y1, xend = x2, yend = y2), col = "darkcyan") + 
+  geom_segment(data = shorter , aes(x = x1, y = y1, xend = x2, yend = y2), col = "darkorange1") 
+#+
+ #  + 
+ # geom_circle(data = data_circle, aes(x0 = x0, y0 = y0, r=r))
 
 # Using Principle of Indifference
 prob = chords_longer/n
 print(prob)
 
-# 1. Select the random endpoints directly from the dataframe
-circle[1:2,]
-
-# 2. Transform tibble to matrix
-make_matrix <- function(df,rownames = NULL){
-  my_matrix <-  as.matrix(df)
-  if(!is.null(rownames))
-    rownames(my_matrix) = rownames
-  my_matrix
-}
-
-make_matrix(circle, rownames = NULL)
-
-
-
 # METHOD B (Random Radial Points) ----------------------------------------------
+library(tidyverse)
+library(ggforce)
+library(ggplot2)
+library(dplyr)
 
+data_circle <- data.frame(x0 = 0,
+                          y0 = 0,
+                          r = 1)
+
+ggplot(data = data_circle, aes(x = x0, y = y0)) + 
+  geom_circle(data = data_circle, aes(x0 = x0, y0 = y0, r=r))
 
 
 # METHOD C (Random Midpoints) --------------------------------------------------
