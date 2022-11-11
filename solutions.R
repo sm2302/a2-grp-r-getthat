03090f76dd71ce73384d1cb381d5127ff4c16cc5
alyaadis <19b4024@ubd.edu.bn>
2022-11-11 21:00:53 +0800

    Updated code
    Deleted unnecessary ones

--- solutions.R
+++ solutions.R
@@ -5,47 +5,6 @@ library(tidyverse)
 library(ggforce)
 library(ggplot2)
 library(dplyr)
-theme_set(theme_void())
-
-# Draw a random chord in a unit circle centred at origin -----------------------
-
-# Coordinates of equilateral triangle
-eqtri_df <- tibble(
-  x    = c(0, sqrt(3) / 2, -sqrt(3) / 2),
-  y    = c(1, -0.5, -0.5),
-  xend = c(sqrt(3) / 2, -sqrt(3) / 2, 0),
-  yend = c(-0.5, -0.5, 1)
-)
-
-# Coordinates of random chord
-rdmchr_df <- tibble(
-  x    = 0.93636368,
-  y    = 0.35103142,
-  xend = -0.9999991,
-  yend = -0.001326758
-)
-
-# Plot
-p <- ggplot() +
-  ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
-  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_segment(data = rdmchr_df, aes(x = x, y = y, xend = xend, yend = yend),
-               col = "red3") +
-  coord_equal()
-
-ggsave(p, file = "plot.png", height = 5, width = 7)
-
-# Plot a circle
-library(ggplot2)
-library(ggforce)
-
-data_circle <- data.frame(x0 = 0,
-                          y0 = 0,
-                          r = 1)
-
-ggplot(data = data_circle, aes(x = x0, y = y0)) + 
-  geom_circle(data = data_circle, aes(x0 = x0, y0 = y0, r=r))
-
 
 # METHOD A (Random Endpoints) --------------------------------------------------
 n <- 200 # number of chords - can be changed
@@ -92,23 +51,22 @@ ggplot(data = endpoint, aes(x = x1, y = y1)) +
 prob = chords_longer/n
 print(prob)
 
-
 # METHOD B (Random Radial Points) ----------------------------------------------
 n <- 150 # number of chords - can be changed
 r <- 1
 
-theta_B <- runif(n, 0, 2*pi) # runif to randomise angles
-P <- runif(n,0,r) # runif to randomise radius
+theta_B <- runif(n, 0, 2*pi) # randomise angles
+P <- runif(n,0,r) # randomise radius
 Q <- sqrt(r^2-P^2) 
 
-# coordinates of random radial points
+# Coordinates of random radial points
 endpoint <- tibble(
   x1 = P*cos(theta_B)+ Q*sin(theta_B),
   y1 = P*sin(theta_B)- Q*cos(theta_B),
   x2 = P*cos(theta_B)- Q*sin(theta_B),
   y2 = P*sin(theta_B)+ Q*cos(theta_B))
 
-# coordinates of equilateral triangle
+# Coordinates of equilateral triangle
 eqtri_df <- tibble(
   x    = c(0, sqrt(3) / 2, -sqrt(3) / 2),
   y    = c(1, -0.5, -0.5),
